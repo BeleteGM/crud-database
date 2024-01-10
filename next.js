a40ae0bcb9d851f19a6dd0b164913fd6c1f027d6
BeleteGM <gmnetwork21@gmail.com>
2024-01-09 23:07:05 -0900

    crud dtabase added

---
+++ next.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const app= express();
+app.listen(1149, (err)=>{
+    if(err){
+        console.log(err);
+
+    }
+    console.log("our server is listining port 1149");
+
+})
